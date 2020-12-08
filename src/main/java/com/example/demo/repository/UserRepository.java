666bf5a1c6fc1eebc09bd87fb29ebd272db7b0ba
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-08 17:59:14 +0500

    optimized imports

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,14 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Role;
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Set;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 @Service
 public class UserService implements UserDetailsService {
