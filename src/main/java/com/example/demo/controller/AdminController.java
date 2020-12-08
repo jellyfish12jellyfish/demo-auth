26518a5fa861beabf1cd349e939cd30bd4ee9b76
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-08 19:35:40 +0500

    optimzed imports

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Arrays;
 import java.util.Set;
 
 @Controller
