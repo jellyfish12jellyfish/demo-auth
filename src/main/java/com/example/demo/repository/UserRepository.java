b21da27c0e37d4ce5528e267f539ca8b10ca3b01
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-16 15:50:44 +0500

    Create a query for update the login time

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,12 +2,20 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("from User where username=:name")
     User findByUsername(@Param("name") String username);
+
+    @Modifying
+    @Transactional
+    @Query("update User u set u.createdAt = current_timestamp where u.username=:name")
+    void setLastLoginTime(@Param("name") String username);
 
 }
