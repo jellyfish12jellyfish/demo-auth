3ae2b3598097a9cec2a6b16c5a5c3a0f569ee1c5
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-12-31 14:50:06 +0500

    Optimize imports

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,15 +4,11 @@ package com.example.demo.controller;
  * Time: 9:39 PM
  * */
 
-import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 
