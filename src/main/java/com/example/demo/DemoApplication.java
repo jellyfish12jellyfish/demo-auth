0786dd540dea0df31afef6d9dc8b958d585c80a6
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-01 10:53:10 +0500

    Add data initialization

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import com.example.demo.entity.Role;
+import com.example.demo.repository.RoleRepository;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    // instead of data.sql script
+    @Bean
+    InitializingBean addRoles() {
+        return () -> {
+            roleRepository.save(new Role(1L, "ROLE_USER"));
+            roleRepository.save(new Role(2L, "ROLE_ADMIN"));
+        };
     }
 
 }
