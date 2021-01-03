8c18c0f7915a0e67170be4a9da7ee096ac3870b3
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-03 10:39:29 +0500

    Rename and update TestClass

--- src/test/java/com/example/demo/AdminControllerTests.java
+++ src/test/java/com/example/demo/AdminControllerTests.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class AdminControllerTests extends HelperClass {
+public class AdminControllerTests extends TestClass {
 
     @Test
     public void testDoAdminLogin() throws Exception {
--- src/test/java/com/example/demo/UserControllerTests.java
+++ src/test/java/com/example/demo/UserControllerTests.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class UserControllerTests extends HelperClass {
+public class UserControllerTests extends TestClass {
 
 
     @Test
