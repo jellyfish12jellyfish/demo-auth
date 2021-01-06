86540eaac9b1ebe4eee1902de535fab5124752bf
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-06 09:50:36 +0500

    Remove data init methods

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,46 +1,11 @@
 package com.example.demo;
 
-import com.example.demo.entity.Role;
-import com.example.demo.repository.RoleRepository;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class DemoApplication {
-
-    @Autowired
-    private RoleRepository roleRepository;
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-    // instead of using the data.sql script
-    @Bean
-    InitializingBean createRolesIfDoNotExist() {
-        return () -> {
-            Role role_user = roleRepository.findByName("ROLE_USER");
-            Role role_admin = roleRepository.findByName("ROLE_ADMIN");
-
-            if (role_user == null && role_admin == null) {
-                roleRepository.save(new Role(1L, "ROLE_USER"));
-                roleRepository.save(new Role(2L, "ROLE_ADMIN"));
-            }
-
-        };
-    }
-
-    @Bean
-    @Profile("test")
-    InitializingBean createRoles() {
-        return () -> {
-            roleRepository.save(new Role(1L, "ROLE_USER"));
-            roleRepository.save(new Role(2L, "ROLE_ADMIN"));
-        };
-    }
-
 }
