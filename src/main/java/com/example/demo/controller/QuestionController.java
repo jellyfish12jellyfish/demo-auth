cd9356e4e67626b9d010e71ad57a10653d5db59e
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-07 10:43:33 +0500

    Implement pagination

--- src/main/java/com/example/demo/controller/QuestionController.java
+++ src/main/java/com/example/demo/controller/QuestionController.java
@@ -4,18 +4,29 @@ package com.example.demo.controller;
  * Time: 8:15 AM
  * */
 
+import com.example.demo.domain.Pager;
+import com.example.demo.entity.Question;
 import com.example.demo.service.QuestionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
+
+    private static final int BUTTONS_TO_SHOW = 5;
+    private static final int INITIAL_PAGE = 0;
+    private static final int INITIAL_PAGE_SIZE = 10;
 
     private static final Logger log = LoggerFactory.getLogger(QuestionController.class.getName());
 
@@ -26,10 +37,23 @@ public class QuestionController {
     }
 
     @GetMapping("/{id}/all")
-    public String allQuestions(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("questions", questionService.getThemeQuestions(id));
+    public ModelAndView allQuestions(@RequestParam("page") Optional<Integer> page,
+                                     @PathVariable("id") Long id) {
 
-        log.info(">>> GET questions.html");
-        return "question/questions";
+        ModelAndView questionPage = new ModelAndView("question/questions");
+
+        int evalPageSize = INITIAL_PAGE_SIZE;
+        int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
+
+        Page<Question> questions =
+                questionService.findAllPageable(id, PageRequest.of(evalPage, evalPageSize));
+        Pager pager = new Pager(questions.getTotalPages(), questions.getNumber(), BUTTONS_TO_SHOW);
+
+        questionPage.addObject("questions", questions);
+        questionPage.addObject("selectedPageSize", evalPageSize);
+        questionPage.addObject("themeId", id);
+        questionPage.addObject("pager", pager);
+
+        return questionPage;
     }
 }
