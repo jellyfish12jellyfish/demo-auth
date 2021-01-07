28a5d3925dc0c106fe78981f64ef583277c130eb
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-07 08:44:38 +0500

    Add QuestionController

--- src/main/java/com/example/demo/controller/QuestionController.java
+++ src/main/java/com/example/demo/controller/QuestionController.java
@@ -4,16 +4,32 @@ package com.example.demo.controller;
  * Time: 8:15 AM
  * */
 
+import com.example.demo.service.QuestionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
 
-    @GetMapping("/{theme}/all")
-    public String allQuestions() {
+    private static final Logger log = LoggerFactory.getLogger(QuestionController.class.getName());
+
+    private final QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @GetMapping("/{id}/all")
+    public String allQuestions(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("questions", questionService.getThemeQuestions(id));
+
+        log.info(">>> GET questions.html");
         return "question/questions";
     }
 }
