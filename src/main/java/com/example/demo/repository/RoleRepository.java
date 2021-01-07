e6fb970a9488772ade37d8def691b781265aef18
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-07 10:50:55 +0500

    Rename commit message

--- src/main/java/com/example/demo/repository/RoleRepository.java
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -4,6 +4,5 @@ import com.example.demo.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-
     Role findByName(String name);
 }
