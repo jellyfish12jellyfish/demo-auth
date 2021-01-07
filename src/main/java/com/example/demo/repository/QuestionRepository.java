d5ba4f7917035c4bcd026a6c41de7b72332b96be
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-07 08:43:33 +0500

    Add query find all by id

--- src/main/java/com/example/demo/repository/QuestionRepository.java
+++ src/main/java/com/example/demo/repository/QuestionRepository.java
@@ -3,10 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
-//    @Modifying
-//    @Transactional
-//    @Query("update Question q set q.lastViewAt = current_timestamp where q.id=:id")
-//    void setLastViewTime(@Param("id") Long id);
+    List<Question> findAllByThemeId(Long id);
 }
