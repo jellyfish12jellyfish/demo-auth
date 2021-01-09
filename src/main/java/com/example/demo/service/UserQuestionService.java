5e03519e9979aefe9adb4084885c99691ec227c0
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-09 10:54:49 +0500

    Add service interface

---
+++ src/main/java/com/example/demo/service/UserQuestionService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Question;
+import com.example.demo.entity.User;
+import com.example.demo.entity.UserQuestion;
+
+import java.util.List;
+
+public interface UserQuestionService {
+
+    void save(UserQuestion userQuestion);
+
+    UserQuestion findByUserAndQuestion(User user, Question question);
+
+    List<UserQuestion> findAllByUserId(Long userId);
+}
