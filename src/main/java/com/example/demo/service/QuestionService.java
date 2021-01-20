2b69aace6fb08eb45f81eaf5ce2e3bb19b4b3fcd
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-01-20 09:03:26 +0500

    Update createOrUpdate method: add Theme

--- src/main/java/com/example/demo/service/QuestionService.java
+++ src/main/java/com/example/demo/service/QuestionService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Question;
+import com.example.demo.entity.Theme;
 import com.example.demo.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,5 +18,5 @@ public interface QuestionService {
 
     void deleteById(Long questionId);
 
-    void createOrUpdate(Question question, User userFromDb);
+    void createOrUpdate(Question question, User userFromDb, Theme theme);
 }
