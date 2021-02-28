f9acf196d455c9b694e149358373c95640810e79
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-02-28 08:15:51 +0500

    Add user verifying methods

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -14,7 +14,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.security.Principal;
 
@@ -33,7 +35,7 @@ public class AuthController {
     @GetMapping("/login")
     public String getLoginPage(Principal principal) {
         log.info(">>> GET login.html");
-        return principal == null ? "registration/login" : "home";
+        return principal == null ? "auth/login" : "home";
     }
 
     // get registration page
@@ -41,25 +43,40 @@ public class AuthController {
     public String getRegistrationPage(@ModelAttribute("user") User user, Principal principal) {
 
         log.info(">>> GET registration.html");
-        return principal == null ? "registration/registration" : "home";
+        return principal == null ? "auth/registration" : "home";
     }
 
     // do registration
     @PostMapping("/registration")
     public String addUser(@Valid @ModelAttribute("user") User user,
+                          HttpServletRequest request,
                           BindingResult bindingResult,
                           Model model) {
 
         boolean fieldHasErrors = userService.checkFieldErrors(user, bindingResult, model);
 
         if (fieldHasErrors) {
-            return "registration/registration";
+            return "auth/registration";
         }
 
-        userService.registerUser(user);
+        String siteUrl = getSiteUrl(request);
+        log.info(">>> SiteUrl after method = {}", siteUrl);
 
-        log.info(">>> GET:redirect login.html");
-        return "redirect:/login";
+        userService.registerUser(user, siteUrl);
+        return "auth/register-success";
+
     }
 
+    // get base url
+    private String getSiteUrl(HttpServletRequest request) {
+        String siteUrl = request.getRequestURL().toString();
+        log.info(">>> SiteUrl = {}", siteUrl);
+
+        return siteUrl.replace(request.getServletPath(), "");
+    }
+
+    @GetMapping("/verify")
+    public String verifyUser(@RequestParam("code") String code) {
+        return userService.verifyUser(code) ? "auth/verify-success" : "auth/verify-fail";
+    }
 }
